2bdc2338bd088d9fcedb32aeb34c8989ba4b4ccf
kanaru-ssk <2019january.0131@gmail.com>
2023-10-28 09:50:07 +0900

    調整パラメータを追加

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,12 +3,18 @@
 //--------------------------------------------------------------
 void ofApp::setup()
 {
-    numSplit = 1024; // 分割数
+    numSplit = 1024;    // 分割数
+    numImages = 3;      // 画像数
+    minDOpacity = 0.01; // 透明度の変化幅の最小値
+    maxDOpacity = 0.03; // 透明度の変化幅の最大値
 
     // 画像ロード
     imagesDir.listDir("images"); // bin/data/images/ フォルダ内のjpg画像を取得
     imagesDir.allowExt("jpg");
-    numImages = imagesDir.size();
+    if (imagesDir.size() < numImages) {
+        numImages = imagesDir.size();
+
+    }
     images.assign(numImages, ofImage());
     for (int i = 0; i < numImages; i++)
     {
@@ -57,10 +63,10 @@ void ofApp::init()
     vector<float> splitData(numSplit*3);
     vector<float> dOpacityData(numSplit);
     for (int i = 0; i < numSplit; i++) {
-        splitData[i*3 + 0] = ofRandom(1.0);          // 現在の画像ID
-        splitData[i*3 + 1] = ofRandom(1.0);          // 次の画像ID
-        splitData[i*3 + 2] = 1.0f;                   // 透明度
-        dOpacityData[i] = ofRandom(0.01, 0.03); // 透明度の変化幅
+        splitData[i*3 + 0] = ofRandom(1.0); // 現在の画像ID
+        splitData[i*3 + 1] = ofRandom(1.0); // 次の画像ID
+        splitData[i*3 + 2] = 1.0f; // 透明度
+        dOpacityData[i] = ofRandom(minDOpacity, maxDOpacity); // 透明度の変化幅
     }
     splitTex.allocate(numSplit, 1, GL_RGB);
     splitTex.src->getTexture().loadData(splitData.data(), numSplit, 1, GL_RGB);
